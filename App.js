ef98d8ab50f730fc35415b4f04ad308c702b8d52
Santiago Iannello <santiagoiannellot@gmail.com>
2024-11-29 19:09:54 -0300

    Proyecto comenzado

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text class="text-red-500 text-xl">Hola mundo!efew</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
