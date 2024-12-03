120176835b3bfab5c8f0143353ed37f3188fe9ed
unknown <santiagoiannellot@gmail.com>
2024-12-03 01:43:06 -0300

    Change to Ionic

--- components/Screen.jsx
+++
@@ -1,9 +0,0 @@
-import { View } from "react-native";
-
-export function Screen({ children }) {
-    return (
-        <View className="flex-1 bg-slate-100 px-4 pt-4">
-            {children}
-        </View>
-    )
-}
